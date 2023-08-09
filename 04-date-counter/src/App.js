87d237904ea78b506ccfc37a2e3112d1e725a511
Schmid Simon <schms27@gmail.com>
2023-08-09 14:23:10 +0200

    use better input elements for date counter component

--- 04-date-counter/src/App.js
+++ 04-date-counter/src/App.js
@@ -19,23 +19,43 @@ function App() {
   const [steps, setSteps] = useState(1);
   const [deltaDays, setDeltaDays] = useState(0);
 
+  function handleReset() {
+    setDeltaDays(0);
+    setSteps(1);
+  }
+
   return (
     <div className="App">
       <div>
-        <button onClick={() => setSteps((s) => s - 1)}>-</button>
-        <span>Steps: {steps}</span>
-        <button onClick={() => setSteps((s) => s + 1)}>+</button>
+        <span>Step: {steps}</span>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={steps}
+          onChange={(e) => setSteps(Number(e.target.value))}
+        />
       </div>
 
       <div>
+        <span>Days: </span>
         <button onClick={() => setDeltaDays((d) => d - steps)}>-</button>
-        <span>Days: {deltaDays}</span>
+        <input
+          value={deltaDays}
+          onChange={(e) => setDeltaDays(Number(e.target.value))}
+        />
         <button onClick={() => setDeltaDays((d) => d + steps)}>+</button>
       </div>
 
       <div>
         {deltaDays} days from today is: <b>{getDate(deltaDays)}</b>
       </div>
+
+      {deltaDays !== 0 || steps !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
