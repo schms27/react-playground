1659c804cf794af5b3aff9c3a48c6a5d4453463e
Schmid Simon <schms27@gmail.com>
2023-08-14 15:38:02 +0200

    add reusable star component

--- 10-use-popcorn/src/index.js
+++ 10-use-popcorn/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+//import "./index.css";
+//import App from "./App";
+
+import StarRating from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <StarRating maxStars={5} defaultRating={4} />
+    <StarRating maxStars={10} size={64} color="blue" />
   </React.StrictMode>
 );
