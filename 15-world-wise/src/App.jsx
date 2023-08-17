f19429e36f40d96855a130a8218d8b67a5b54df1
Schmid Simon <schms27@gmail.com>
2023-08-17 15:59:50 +0200

    add first vite and spa example app

---
+++ 15-world-wise/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hellooooooo</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
