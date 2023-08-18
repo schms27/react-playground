888b222f018d6bfc5181e038ecb0e6d02bde3de2
Schmid Simon <schms27@gmail.com>
2023-08-18 09:27:28 +0200

    add app sub-navigation

--- 15-world-wise/src/App.jsx
+++ 15-world-wise/src/App.jsx
@@ -9,14 +9,18 @@ import AppLayout from "./pages/AppLayout";
 function App() {
   return (
     <div>
-      <h1>Hellooooooo</h1>
       <BrowserRouter>
         <Routes>
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
-          <Route path="/" element={<Homepage />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List of cities</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
+          <Route index element={<Homepage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
