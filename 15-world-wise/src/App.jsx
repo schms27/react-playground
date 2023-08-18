756d16f1952aa56a93655c7ed9a0209e6fff47ee
Schmid Simon <schms27@gmail.com>
2023-08-18 07:19:46 +0200

    add more pages and pre-defined styles

--- 15-world-wise/src/App.jsx
+++ 15-world-wise/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="/" element={<Homepage />} />
           <Route path="*" element={<PageNotFound />} />
