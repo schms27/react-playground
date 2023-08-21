7510cd26ff89955bac29f59f44f212ebdcc61f61
Schmid Simon <schms27@gmail.com>
2023-08-21 10:27:19 +0200

    add leaflet map

--- 15-world-wise/src/components/Map.jsx
+++ 15-world-wise/src/components/Map.jsx
@@ -1,11 +1,16 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import styles from "./Map.module.css";
+import { useState } from "react";
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition] = useState([40, 0]);
 
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  console.log(lat + lng);
   return (
     <div
       className={styles.mapContainer}
@@ -13,10 +18,22 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
