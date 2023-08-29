59ed5342abb42fc7a7a719ab1e5ad38576a7818f
Schmid Simon <schms27@gmail.com>
2023-08-29 13:33:49 +0200

    performance improvements

--- 16-atomic-blog/src/App.js
+++ 16-atomic-blog/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, createContext, useContext } from "react";
 import { faker } from "@faker-js/faker";
 
+import Test from "./Test";
 import { PostProvider, usePosts } from "./PostContext";
 
 function createRandomPost() {
@@ -122,14 +123,17 @@ function FormAddPost() {
 function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      <Test />
+    </>
   );
 }
 
