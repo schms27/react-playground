d1307a1755c1c9ceee320054f51ce13e83e5efe2
Schmid Simon <schms27@gmail.com>
2023-08-30 15:21:12 +0200

    use reduxjs/toolkit instead of react-redux

--- 19-redux-intro/src/features/customers/customerSlice.js
+++ 19-redux-intro/src/features/customers/customerSlice.js
@@ -1,33 +1,33 @@
-const initialStateCustomer = { fullName: "", nationalID: "", createdAt: "" };
+import { createSlice } from "@reduxjs/toolkit";
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-      };
-    default:
-      return state;
-  }
-}
+const initialState = { fullName: "", nationalID: "", createdAt: "" };
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-export function updateName(fullName) {
-  return {
-    type: "customer/updateName",
-    payload: { fullName },
-  };
-}
+const customerSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload.fullName;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
--- 19-redux-intro/src/store.js
+++ 19-redux-intro/src/store.js
@@ -1,17 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunk from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
