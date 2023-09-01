ae94badf2fe2e1c0009a6296adab71653a239572
Schmid Simon <schms27@gmail.com>
2023-09-01 14:39:09 +0200

    add some basic styling

--- 20-fast-react-pizza/src/ui/AppLayout.jsx
+++ 20-fast-react-pizza/src/ui/AppLayout.jsx
@@ -8,15 +8,17 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
       {isLoading && <LoadingOverlay />}
 
       <Header />
 
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
       <CartOverview />
     </div>
   );
--- 20-fast-react-pizza/src/ui/Header.jsx
+++ 20-fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/"> Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between border-b border-stone-400 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link className="tracking-widest" to="/">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Simon</p>
+      <Username />
     </header>
   );
 }
